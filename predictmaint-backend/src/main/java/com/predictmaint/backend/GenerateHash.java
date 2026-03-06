bcbca85f09c11b13b885b4d7cc4eb5460687db61
jesusartemio-dev <jesusartemiodev@gmail.com>
2026-03-06 11:50:38 -0500

    fix: limpiar node_modules y GenerateHash del repo

--- predictmaint-backend/src/main/java/com/predictmaint/backend/GenerateHash.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-public class GenerateHash {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("1234"));
-    }
-}
